63f5a7be67ac4592bcde2b3bf534e035f1bd168f
Colum <Colum@DESKTOP-JLGFCL0>
2017-10-30 14:20:08 +0000

    ...

---
+++ src/AccountManager/AccountFactory.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AccountManager;
+
+/**
+ *
+ * @author Colum
+ */
+public class AccountFactory {
+    
+    public Account getAccount(String name, double balance){
+        if(balance < 500){
+            return new BronzeUser(name, balance);
+        }
+        else if (balance < 1500){
+            return new SilverUser(new BronzeUser(name, balance));
+        }
+        else if (balance < 3500){
+            return new GoldUser(new BronzeUser(name, balance));
+        }
+        else if (balance < 7500){
+            return new PlatinumUser(new BronzeUser(name, balance));
+        }
+        else 
+            return new DiamondUser(new BronzeUser(name, balance));
+    }
+}
